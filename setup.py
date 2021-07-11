42584b8f90338023b7dc39f0c4a037c052a9a0f1
philip928lin <philip928lin@gmail.com>
2021-07-11 16:31:06 -0400

    Update setup

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 import setuptools
 
 setuptools.setup(name='pyramid',
-                 version='1.0.1',
+                 version='1.0.0',
                  description='A python package of a Riverware and Agent-based Modeling Interface for Developers.',
                  url='',
                  author='Chung-Yi Lin',
                  author_email='philip928lin@gmail.com',
-                 license='NotOpenYet',
+                 license='MIT',
                  packages = setuptools.find_packages(),
                  install_requires = ['matplotlib>=3.0.3,<3.1.0', 'numpy>=1.19', 'pandas>=1.1', 'scipy>=1.5', 'tqdm', 'geneticalgorithm','joblib'],
                  zip_safe=False,
